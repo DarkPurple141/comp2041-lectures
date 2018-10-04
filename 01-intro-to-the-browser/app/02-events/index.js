84475843325409f202f67aedf663b8c5e46be418
Alex Hinds <alex.hinds141@gmail.com>
2018-10-04 17:02:46 +1000

    lecture 2

--- 01-intro-to-the-browser/app/02-events/index.js
+++
@@ -1,8 +0,0 @@
-const timerEl = document.getElementsByClassName('timer')[0];
-const startButtonEl = document.getElementById('startButton');
-
-function onStartClick() {
-  console.log('Start was clicked');
-}
-
-startButtonEl.addEventListener('click', onStartClick);
